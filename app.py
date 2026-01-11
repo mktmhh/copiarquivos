4d37a54b402aa91763261f71d8e30e80fafa341c
mktmhh <mktdigitalmhh@gmail.com>
2026-01-11 11:24:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,3 +25,7 @@ def copiar():
         return jsonify({"ok": True})
     except Exception as e:
         return jsonify({"erro": str(e)}), 500
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
